0c305b29964713c6bf8b766253ae8d7559cfcf78
Mritunjay <ab@c.com>
2026-02-25 15:26:30 +0530

    todo app get all, get by id and create todo

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -7,6 +7,17 @@ import { getStoredTodos, saveTodos } from "../helpers/todoHelpers.js";
 export const getTodos = (_req: Request, res: Response) => {
     res.status(200).json(getStoredTodos());
 }
+// Get todo by id
+export const getTodo = (req: Request, res: Response) => {
+    const todos = getStoredTodos();
+    
+    const id = Number(req.params.id);
+    const todo = todos.find(t => t.id === id);
+    if(!todo) {
+        return res.status(404).send({ msg: "Todo not available" });
+    }
+    res.status(200).json({ msg: "Todo found", data: todo });
+}
 // Create todo
 export const createTodo = (req: Request, res: Response) => {
     const todos = getStoredTodos();
@@ -19,4 +30,17 @@ export const createTodo = (req: Request, res: Response) => {
     todos.push(newTodo);
     saveTodos(todos);
     res.status(201).json(newTodo);
+}
+
+// Toggle completes
+export const toggleTodo = (req: Request, res: Response) => {
+    const todos = getStoredTodos();
+    const id = Number(req.params.id);
+    const todo = todos.find(t => t.id === id);
+    if(!todo) {
+        return res.status(404).send({ msg: "Task not available" });
+    }
+    todo.completed = !todo.completed;
+    saveTodos(todos);
+    res.status(202).json(todo);
 }
