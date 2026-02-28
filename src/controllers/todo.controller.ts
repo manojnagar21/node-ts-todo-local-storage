dcb155ab384e5eac1571ac4922822c842fca01e9
Mritunjay <ab@c.com>
2026-02-28 15:00:32 +0530

    try catch block added in getTodo api

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from "express";
 import { localStorage } from "../storage/localstorage.js";
 import type { Todo } from "../types/todo.js";
 import { getStoredTodos, saveTodos } from "../helpers/todoHelpers.js";
-import { createTodoSchema } from "../validations/todo.validation.js";
+import { createTodoSchema, idParamSchema } from "../validations/todo.validation.js";
 import { z } from "zod";
 
 // Get all todos
@@ -11,27 +11,31 @@ export const getTodos = (_req: Request, res: Response) => {
 };
 // Get todo by id
 export const getTodo = (req: Request, res: Response) => {
-    const todos = getStoredTodos();
-    
-    const id = Number(req.params.id);
-    const todo = todos.find(t => t.id === id);
-    if(!todo) {
-        return res.status(404).send({ msg: "Todo not available", data: [] });
+    // try catch
+    try {
+        const todos = getStoredTodos();
+        const id = Number(req.params.id);
+        const todo = todos.find(t => t.id === id);
+        if(!todo) {
+            return res.status(404).send({ msg: "Todo not available", data: [] });
+        }
+        res.status(200).json({ msg: "Todo found", data: todo });
+    } catch (error) {
+        res.status(500).json({ msg: "Something went wrong", data: [] });
     }
-    res.status(200).json({ msg: "Todo found", data: todo });
 };
 // Create todo
 export const createTodo = (req: Request, res: Response) => {
     const todos = getStoredTodos();
-    // try {
-    // validate request
+    // try catch
     try {
+        // validate request
         const validated = createTodoSchema.safeParse(req.body);
         if(!validated.success) {
             const flattened = z.flattenError(validated.error);
             return res.status(400).json(flattened.fieldErrors);
         }
-        const data = validated.data
+        const data = validated.data;
         const newTodo: Todo = {
             id: Date.now(),
             title: data.title,
